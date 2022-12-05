7f054cf20d9f89b2771951e31c04e31bc441ec60
Joshua <joshua537.nit@gmail.com>
2022-12-05 14:07:01 +0530

    Create coroutine_3.kt

---
+++ coroutine_3.kt
@@ -0,0 +1,13 @@
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    doWorld()
+}
+
+suspend fun doWorld() = coroutineScope {  // this: CoroutineScope
+    launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello")
+}
